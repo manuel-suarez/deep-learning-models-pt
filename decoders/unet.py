65a87658fa3a5e876a39e5fbf62d1c1b24825b70
Arturo Suárez <manuel.suarez@cimat.mx>
2024-08-11 12:16:26 -0600

    Added center block (without skip connection) for vgg encoder arch

--- decoders/unet.py
+++ decoders/unet.py
@@ -24,6 +24,18 @@ class Conv2dReLU(nn.Module):
         return x
 
 
+class CenterBlock(nn.Module):
+    def __init__(self, kernels_in, kernels_out, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.conv2drelu1 = Conv2dReLU(kernels_in, kernels_out)
+        self.conv2drelu2 = Conv2dReLU(kernels_out, kernels_out)
+
+    def forward(self, x):
+        x = self.conv2drelu1(x)
+        x = self.conv2drelu2(x)
+        return x
+
+
 class DecoderBlock(nn.Module):
     def __init__(self, kernels_in, kernels_out):
         super().__init__()
@@ -44,10 +56,15 @@ class UnetDecoder(nn.Module):
         self,
         inputs=[3072, 768, 384, 128, 32],
         outputs=[256, 128, 64, 32, 16],
+        has_center=False,
+        center_size=512,
         *args,
         **kwargs
     ) -> None:
         super().__init__(*args, **kwargs)
+        self.has_center = has_center
+        if has_center:
+            self.center_block = CenterBlock(center_size, center_size)
         self.decoder_block1 = DecoderBlock(inputs[0], outputs[0])
         self.decoder_block2 = DecoderBlock(inputs[1], outputs[1])
         self.decoder_block3 = DecoderBlock(inputs[2], outputs[2])
@@ -56,6 +73,8 @@ class UnetDecoder(nn.Module):
 
     def forward(self, x, skips):
         c1, c2, c3, c4 = skips
+        if self.has_center:
+            x = self.center_block(x)
         x = self.decoder_block1(x, c4)
         x = self.decoder_block2(x, c3)
         x = self.decoder_block3(x, c2)
