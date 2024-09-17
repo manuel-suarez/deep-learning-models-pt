e3d318577912bc682f619560f709fac38c090cc4
Arturo Suárez <manuel.suarez@cimat.mx>
2024-09-17 16:36:14 -0600

    Adding network parameters

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from models.unet_resnet34 import UnetResNet34
 
 
-def get_model(arch, encoder=None):
+def get_model(arch, args, encoder=None):
     if arch == "unet":
         if encoder == "resnet34":
-            return UnetResNet34()
+            return UnetResNet34(**args)
