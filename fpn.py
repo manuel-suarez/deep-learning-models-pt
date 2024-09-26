1732017a289f6caaf35d781bbcdf0cfea798e5c5
Arturo Suárez <manuel.suarez@cimat.mx>
2024-09-26 16:49:45 -0600

    Generic model for FPN

--- fpn.py
+++ fpn.py
@@ -5,14 +5,18 @@ from segmentation.segmentationhead import SegmentationHead
 
 
 class FPN(nn.Module):
-    def __init__(self, encoder_name, activation=False) -> None:
+    def __init__(
+        self, encoder_name, in_channels=3, out_channels=1, activation=False
+    ) -> None:
         super().__init__()
         ## Encoder
-        self.encoder = get_encoder(encoder_name)
+        self.encoder = get_encoder(encoder_name, in_channels=in_channels)
         ## Decoder
         self.decoder = FPNDecoder(**get_fpn_decoder_params(encoder_name))
         ## Segmentation
-        self.segmentation = SegmentationHead(128, activation)
+        self.segmentation = SegmentationHead(
+            kernels_in=128, out_channels=out_channels, has_activation=activation
+        )
 
     def forward(self, inputs):
         _, c2, c3, c4, c5 = self.encoder(inputs)
