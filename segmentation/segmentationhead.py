4288089c3dd299ae9db865ed98a44372f54200a0
Arturo Suárez <manuel.suarez@cimat.mx>
2024-11-13 00:14:58 -0600

    Added block needed for deeplabv3plus arch

--- segmentation/segmentationhead.py
+++ segmentation/segmentationhead.py
@@ -4,7 +4,13 @@ from .common import Activation
 
 class SegmentationHead(nn.Module):
     def __init__(
-        self, kernels_in=16, out_channels=1, has_activation=True, *args, **kwargs
+        self,
+        kernels_in=16,
+        out_channels=1,
+        has_activation=True,
+        has_upsampling=False,
+        *args,
+        **kwargs
     ) -> None:
         super().__init__(*args, **kwargs)
         self.conv = nn.Conv2d(
@@ -16,8 +22,11 @@ class SegmentationHead(nn.Module):
             bias=False,
         )
         self.has_activation = has_activation
+        self.has_upsampling = has_upsampling
         if self.has_activation:
             self.act = Activation()
+        if self.has_upsampling:
+            self.upsampling = nn.UpsamplingBilinear2d(scale_factor=4.0, mode="bilinear")
 
     def forward(self, x):
         x = self.conv(x)
